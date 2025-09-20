04371464958d3a3c657a53842117844d8351ea63
Eetu Lämsä <lamsaeetu01@gmail.com>
2025-09-20 17:48:17 +0300

    Update leds.cpp

--- leds.cpp
+++ leds.cpp
@@ -3,34 +3,55 @@
 
 void initializeLeds()
 {
-// see requirements for this function from leds.h
+ pinMode(A2, OUTPUT);
+ pinMode(A3, OUTPUT);
+ pinMode(A4, OUTPUT);
+ pinMode(A5, OUTPUT);
+
+ digitalWrite(A2, LOW);
+ digitalWrite(A3, LOW);
+ digitalWrite(A4, LOW);
+ digitalWrite(A5, LOW);
 }
 
 void setLed(byte ledNumber)
 {
-// see requirements for this function from leds.h
-
+clearAllLeds();
+ 
+ if (ledeNumber == 0) digitalWrite(A2, HIGH);
+ else if (ledNumber == 1) digitalWrite(A3, HIGH);
+ else if (ledNumber == 2) digitalWrite(A4, HIGH);
+ else if (ledNumber == 3) digitalWrite(A5, HIGH);
 }
 
 
 void clearAllLeds()
 {
-// see requirements for this function from leds.h
- 
+ digitalWrite(A2, LOW);
+ digitalWrite(A3, LOW);
+ digitalWrite(A4, LOW);
+ digitalWrite(A5, LOW); 
 }
 
 void setAllLeds()
 {
-// see requirements for this function from leds.h
+ digitalWrite(A2, HIGH);
+ digitalWrite(A3, HIGH);
+ digitalWrite(A4, HIGH);
+ digitalWrite(A5, HIGH);
 }
 
 
 void show1()
 {
-// see requirements for this function from leds.h
+ for (int value = 0; value < 16; value++) {
+  digitalWrite(A2, (value & 0x01) ? HIGH : LOW);
+  digitalWrite(A3, (value & 0x02) ? HIGH : LOW);
+  digitalWrite(A4, (value & 0x04) ? HIGH : LOW);
+  digitalWrite(A5, (value & 0x08) ? HIGH : LOW);
+   delay(300);
 }
 
 void show2(int rounds)
 {
-// see requirements for this function from leds.h  
 }
