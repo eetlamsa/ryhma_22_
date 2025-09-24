33e253a77bf94b75fc2adbaeff35145c1a00e002
TaraMariaT <tara.taipale@hotmail.com>
2025-09-24 15:51:14 +0300

    Implement initial SpedenSpelit game logic

--- SpedenSpelit.ino
+++ SpedenSpelit.ino
@@ -1,4 +1,3 @@
-#include "display.h"
 #include "buttons.h"
 #include "leds.h"
 #include "SpedenSpelit.h"
@@ -8,56 +7,101 @@
 volatile int buttonNumber = -1;           // for buttons interrupt handler
 volatile bool newTimerInterrupt = false;  // for timer interrupt handler
 
+// game state variables
+int score = 0;
+int expectedIndex = 0;  // where we are in ledSequence
+
 
 void setup()
 {
-  /*
-    Initialize here all modules
-  */
+  initializeLeds();
+  initButtonsAndButtonInterrupts();
+  //initializeDisplay();
+  initializeTimer();
+  sei(); // enable interrupts
 }
 
 void loop()
 {
-  if(buttonNumber>=0)
-  {
-     // start the game if buttonNumber == 4
-     // check the game if 0<=buttonNumber<4
-  }
+  // button pressed?
+  if (buttonNumber >= 0) {
+    int pressed = buttonNumber;
+    buttonNumber = -1;
 
-  if(newTimerInterrupt == true)
-  {
-     // new random number must be generated
-     // and corresponding let must be activated
+    if (pressed == 6) { // start button
+      startTheGame();
+    } else if (pressed >= 2 && pressed <= 5) {
+      // map pins 2..5 -> led numbers 0..3
+      byte btn = pressed - 2;
+      checkGame(btn);
+    }
+  }
+  
+  // timer ticked?
+  if (newTimerInterrupt) {
+    newTimerInterrupt = false;
+
+    // pick new led, store to sequence, light it
+    byte rnd = random(0, 4);
+    ledSequence[sequenceLength++] = rnd;
+    setLed(rnd);
+
+    expectedIndex = 0; // restart remembering from beginning
   }
 }
 
+// initialize Timer1 to 1 Hz but disabled at start
 void initializeTimer(void)
 {
-	// see requirements for the function from SpedenSpelit.h
+  TCCR1A = 0; // normal mode
+  TCCR1B = 0;
+  OCR1A = 15624;  // (16e6 / 1024) - 1 = 1 Hz
+  TCCR1B |= (1 << WGM12);  // CTC mode
+  TIMSK1 |= (1 << OCIE1A); // enable compare interrupt
+  // no prescaler yet, will start in startTheGame()
 }
+
 ISR(TIMER1_COMPA_vect)
 {
-  /*
-  Communicate to loop() that it's time to make new random number.
-  Increase timer interrupt rate after 10 interrupts.
-  */
-  
+  static int tickCount = 0;
+  tickCount++;
+  newTimerInterrupt = true;
 }
 
-
-void checkGame(byte nbrOfButtonPush)
+// check if player's button is correct
+void checkGame(byte btn)
 {
-	// see requirements for the function from SpedenSpelit.h
-}
+  if (btn == ledSequence[expectedIndex]) {
+    expectedIndex++;
+    score++;
+    // showResult(score);
 
+    // finished current sequence, wait for timer to add new one
+    if (expectedIndex >= sequenceLength) {
+      expectedIndex = 0;
+    }
+  } else {
+    // wrong button → game over
+    setAllLeds();
+    delay(1000);
+    clearAllLeds();
+    TIMSK1 &= ~(1 << OCIE1A); // disable timer
+  }
+}
 
 void initializeGame()
 {
-	// see requirements for the function from SpedenSpelit.h
+  sequenceLength = 0;
+  score = 0;
+  expectedIndex = 0;
+  clearAllLeds();
+  //showResult(0);
 }
 
 void startTheGame()
 {
-   // see requirements for the function from SpedenSpelit.h
+  initializeGame();
+  TCNT1 = 0;
+  TCCR1B |= (1 << CS12) | (1 << CS10); // start timer with 1024 prescaler
 }
 
