e3d546b02bcae224d92a0a2fd4a8a875b863a70f
Eetu Lämsä <lamsaeetu01@gmail.com>
2025-09-28 19:17:48 +0300

    Update timer.cpp

--- timer.cpp
+++ timer.cpp
@@ -1,9 +1,9 @@
 #include "timer.h"
 
-// Tämän muuttujan tarvitsee olla määritelty täällä
+// Määritellään globaalit muuttujat
 volatile bool newTimerInterrupt = false;
+static volatile unsigned long tickCount = 0;  // käytetään ISR:ssä
 
-// Timerin alustus, mutta EI vielä käynnistystä
 void initializeTimer(void)
 {
   TCCR1A = 0; // normaali tila
@@ -11,26 +11,34 @@ void initializeTimer(void)
   OCR1A = 15624;               // (16e6 / 1024) - 1 = 1 Hz
   TCCR1B |= (1 << WGM12);      // CTC-tila
   TIMSK1 |= (1 << OCIE1A);     // vertailu-interrupt päälle
-  // ei prescaleria vielä
 }
 
-// Timerin käynnistys (prescaler 1024)
 void startTimer(void)
 {
   TCNT1 = 0;
-  TCCR1B |= (1 << CS12) | (1 << CS10);
+  TCCR1B |= (1 << CS12) | (1 << CS10);  // prescaler 1024
 }
 
-// Timerin pysäytys
 void stopTimer(void)
 {
-  TIMSK1 &= ~(1 << OCIE1A);  // estetään keskeytys
-  TCCR1B &= ~((1 << CS12) | (1 << CS10)); // prescalerit pois → pysähtyy
+  TIMSK1 &= ~(1 << OCIE1A);              // estetään keskeytys
+  TCCR1B &= ~((1 << CS12) | (1 << CS10));// prescaler pois
 }
 
-// ISR itse .cpp:ssä
+// Keskeytys: kasvatetaan tickCountia ja merkataan tapahtuma
 ISR(TIMER1_COMPA_vect)
 {
+  tickCount++;
   newTimerInterrupt = true;
 }
 
+// Rajapinta tickCountin lukemiseen ja nollaamiseen
+unsigned long getTickCount(void)
+{
+  return tickCount;
+}
+
+void resetTickCount(void)
+{
+  tickCount = 0;
+}
