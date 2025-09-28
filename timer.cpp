ffb7de38e8e90e849ba1d4780bf094fef05a88f0
Eetu Lämsä <lamsaeetu01@gmail.com>
2025-09-28 19:11:20 +0300

    Update timer.cpp

--- timer.cpp
+++ timer.cpp
@@ -1,41 +1,36 @@
 #include "timer.h"
-#include "leds.h"
-#include "buttons.h"
 
+// Tämän muuttujan tarvitsee olla määritelty täällä
+volatile bool newTimerInterrupt = false;
 
-void timer1_init(uint16_t compareMatch, uint16_t prescaler) {
-  cli(); //keskeytykset pois
-  TCCR1A = 0;
+// Timerin alustus, mutta EI vielä käynnistystä
+void initializeTimer(void)
+{
+  TCCR1A = 0; // normaali tila
   TCCR1B = 0;
-  TCNT1  = 0;
-
-  OCR1A = compareMatch;      // vertailuarvo
-  TCCR1B |= (1 << WGM12);    
-
-  // prescaler-valinta
-  switch (prescaler) {
-    case 1:    TCCR1B |= (1 << CS10); break;
-    case 8:    TCCR1B |= (1 << CS11); break;
-    case 64:   TCCR1B |= (1 << CS11) | (1 << CS10); break;
-    case 256:  TCCR1B |= (1 << CS12); break;
-    case 1024: TCCR1B |= (1 << CS12) | (1 << CS10); break;
-    default:   TCCR1B |= (1 << CS12); // oletus 256
-  }
-
-  TIMSK1 |= (1 << OCIE1A);   
-  sei(); // keskeytys päälle
+  OCR1A = 15624;               // (16e6 / 1024) - 1 = 1 Hz
+  TCCR1B |= (1 << WGM12);      // CTC-tila
+  TIMSK1 |= (1 << OCIE1A);     // vertailu-interrupt päälle
+  // ei prescaleria vielä
 }
 
-void timer1_stop() {
-  TCCR1B = 0;
-  TIMSK1 &= ~(1 << OCIE1A);
+// Timerin käynnistys (prescaler 1024)
+void startTimer(void)
+{
+  TCNT1 = 0;
+  TCCR1B |= (1 << CS12) | (1 << CS10);
 }
 
-// ISR: nappien ja ledien vertailu
-ISR(TIMER1_COMPA_vect) {
-  updateButtons();  // päivitä nappirekisteri
-  if (ledRegister != 0 && ledRegister == buttonRegister) 
- 
-  
+// Timerin pysäytys
+void stopTimer(void)
+{
+  TIMSK1 &= ~(1 << OCIE1A);  // estetään keskeytys
+  TCCR1B &= ~((1 << CS12) | (1 << CS10)); // prescalerit pois → pysähtyy
+}
+
+// ISR itse .cpp:ssä
+ISR(TIMER1_COMPA_vect)
+{
+  newTimerInterrupt = true;
 }
 
